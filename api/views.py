cf863f75e0f6f30b468fb6b7c9a6bb8540f98217
cbmosley <cbmosley@ncsu.edu>
2022-09-16 15:08:24 -0400

    Added query set filter to note detail and tracker detail so the user only sees their posts.

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Book, BookTracker, BookNote, CustomUser
 from .serializers import BookSerializer, TrackerSerializer, BookNoteSerializer
 from rest_framework.decorators import api_view
@@ -32,6 +32,12 @@ class TrackerList(generics.ListCreateAPIView):
 class TrackerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BookTracker.objects.all()
     serializer_class = TrackerSerializer
+    # permission_classes = (
+    # permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user.id).order_by('book')
 
 
 class BookNoteList(generics.ListCreateAPIView):
@@ -42,12 +48,18 @@ class BookNoteList(generics.ListCreateAPIView):
         queryset = super().get_queryset()
         return queryset.filter(user=self.request.user.id).order_by('book')
 
+
 class BookNoteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BookNote.objects.all()
     serializer_class = BookNoteSerializer
+    # permission_classes = (IsOwnerOrReadOnly)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user.id).order_by('book')
 
 
-@api_view(['GET'])
+@ api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         'books': reverse('book-list', request=request, format=format),
